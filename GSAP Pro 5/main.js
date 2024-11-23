f67addbe10394401540dbb1be76d451e04595b08
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2024-11-23 23:46:51 +0500

    GSAP Pro 5

--- GSAP Pro 5/main.js
+++ GSAP Pro 5/main.js
@@ -1,27 +1,77 @@
-import 'remixicon/fonts/remixicon.css'
-import { gsap } from "gsap";
+import "remixicon/fonts/remixicon.css";
+import gsap from "gsap";
 
-let openMenuBtn = document.querySelector("#open-menu");
-let closeMenuBtn = document.querySelector("#close-menu");
-let navbar = document.querySelector("#navbar");
-let sideBar = document.querySelector("#side-bar");
+
+const openMenuBtn = document.querySelector("#open-menu-btn");
+const sideBar = document.querySelector("#side-bar");
+const closeMenuBtn = document.querySelector("#close-menu");
 
 openMenuBtn.addEventListener("click", () => {
-    // let tl = gsap.timeline()
-    gsap.to(sideBar, {
-        duration: .5,
-        left: "0%",
-    })
-
-    
+  document.getElementById(sideBar.id).classList.remove("hidden");
+  gsap.to(sideBar, {
+    duration: 0.5,
+    left: "0%",
+  });
 });
 
 closeMenuBtn.addEventListener("click", () => {
-    // let tl = gsap.timeline()
-    gsap.to(sideBar, {
-        duration: .5,
-        left: "100%",
-    })
-    console.log("clicked");
-    
+  gsap.to(sideBar, {
+    duration: 0.5,
+    left: "100%",
+  });
+});
+
+gsap.from('#lg-menu h4',{
+  y: -100,
+  ease: "elastic.inOut(1, 0.5)",
+  duration: .5,
+  opacity: 0,
+  stagger: .2
 })
+
+gsap.from('#center-part1 h1',{
+  duration: 1.5,
+  x: "-100%",
+  opacity: 0,
+})
+
+gsap.from('#center-part2 img',{
+  x: "100%",
+  opacity: 0,
+  duration: 1.5,
+})
+
+gsap.from('#center-part1 p',{
+  x: "-100%",
+  opacity: 0,
+  duration: 1.5,
+})
+
+gsap.from('#center-part1 button',{
+  x: "-100%",
+  opacity: 0,
+  duration: 1
+})
+
+gsap.from('#marquee', {
+  y: "150%",
+  opacity: 0,
+  duration: 1
+})
+
+function infiniteLoop() {
+  const marquee = document.querySelector("#marquee")
+  for (let i = 0; i < marquee.children.length; i++) {
+    gsap.to("#item" + (i + 1), {
+      x: `-=${marquee.offsetWidth}`,
+      left: "-13rem",
+      ease: "linear",
+      repeat: -1,
+      duration: 15,
+      delay: - ( 15 / marquee.children.length ) * i
+    })
+  }
+  
+}
+
+infiniteLoop();
