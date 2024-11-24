0ec4970dc8d833c116e8dc0c7f0e7d50adf7766f
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2024-11-25 00:30:56 +0500

    GSAP Pro 5

--- GSAP Pro 5/main.js
+++ GSAP Pro 5/main.js
@@ -1,7 +1,6 @@
 import "remixicon/fonts/remixicon.css";
 import gsap from "gsap";
 
-
 const openMenuBtn = document.querySelector("#open-menu-btn");
 const sideBar = document.querySelector("#side-bar");
 const closeMenuBtn = document.querySelector("#close-menu");
@@ -21,46 +20,46 @@ closeMenuBtn.addEventListener("click", () => {
   });
 });
 
-gsap.from('#lg-menu h4',{
+gsap.from("#lg-menu h4", {
   y: -100,
   ease: "elastic.inOut(1, 0.5)",
-  duration: .5,
+  duration: 0.5,
   opacity: 0,
-  stagger: .2
-})
+  stagger: 0.2,
+});
 
-gsap.from('#center-part1 h1',{
+gsap.from("#center-part1 h1", {
   duration: 1.5,
   x: "-100%",
   opacity: 0,
-})
+});
 
-gsap.from('#center-part2 img',{
+gsap.from("#center-part2 img", {
   x: "100%",
   opacity: 0,
   duration: 1.5,
-})
+});
 
-gsap.from('#center-part1 p',{
+gsap.from("#center-part1 p", {
   x: "-100%",
   opacity: 0,
   duration: 1.5,
-})
+});
 
-gsap.from('#center-part1 button',{
+gsap.from("#center-part1 button", {
   x: "-100%",
   opacity: 0,
-  duration: 1
-})
+  duration: 1,
+});
 
-gsap.from('#marquee', {
+gsap.from("#marquee", {
   y: "150%",
   opacity: 0,
-  duration: 1
-})
+  duration: 1,
+});
 
 function infiniteLoop() {
-  const marquee = document.querySelector("#marquee")
+  const marquee = document.querySelector("#marquee");
   for (let i = 0; i < marquee.children.length; i++) {
     gsap.to("#item" + (i + 1), {
       x: `-=${marquee.offsetWidth}`,
@@ -68,10 +67,99 @@ function infiniteLoop() {
       ease: "linear",
       repeat: -1,
       duration: 15,
-      delay: - ( 15 / marquee.children.length ) * i
-    })
+      delay: -(15 / marquee.children.length) * i,
+    });
   }
-  
 }
 
 infiniteLoop();
+
+function processesAnimation() {
+  const processes = document.getElementById("processes");
+
+  for (let i = 1; i <= processes.children.length; i++) {
+    const process = document.getElementById("process" + i);
+
+    process.addEventListener("click", () => {
+      const process = document.getElementById("process" + i);
+      for (let j = 1; j <= processes.children.length; j++) {
+        const process = document.getElementById("process" + j);
+        const add = document.getElementById("process" + j + "-add");
+        const sub = document.getElementById("process" + j + "-sub");
+
+        if (process !== document.getElementById("process" + i)) {
+          gsap.to(process, {
+            duration: 1,
+            height: "12.5rem",
+            backgroundColor: "#f3f3f3",
+          });
+          gsap.to(add, {
+            duration: 1,
+            rotation: 0,
+            opacity: 1,
+            display: "inline-block",
+          });
+          gsap.to(sub, {
+            display: "none",
+            opacity: 0,
+            duration: 1,
+            rotation: 0,
+          });
+
+          process.classList.add("overflow-y-hidden");
+        }
+      }
+
+      const add = document.getElementById("process" + i + "-add");
+      const sub = document.getElementById("process" + i + "-sub");
+
+      if (
+        process.classList.contains("overflow-y-hidden") &&
+        process === document.getElementById("process" + i)
+      ) {
+        gsap.to(add, {
+          duration: 1,
+          rotation: 180,
+          opacity: 0,
+          display: "none",
+        });
+        gsap.to(sub, {
+          display: "inline-block",
+          opacity: 1,
+          duration: 1,
+          rotation: 180,
+        });
+        gsap.to(process, {
+          duration: 1,
+          height: "auto",
+          backgroundColor: "#b9ff66",
+          onComplete: () => {
+            process.classList.remove("overflow-y-hidden");
+          },
+        });
+      } else {
+        gsap.to(process, {
+          duration: 1,
+          backgroundColor: "#f3f3f3",
+          height: "12.5rem",
+        });
+        gsap.to(add, {
+          duration: 1,
+          rotation: 0,
+          opacity: 1,
+          display: "inline-block",
+        });
+        gsap.to(sub, {
+          display: "none",
+          opacity: 0,
+          duration: 1,
+          rotation: 0,
+        });
+
+        process.classList.add("overflow-y-hidden");
+      }
+    });
+  }
+}
+
+processesAnimation();
